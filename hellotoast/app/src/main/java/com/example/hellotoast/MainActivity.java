98983759a307eecb19241d3fceeef0eb776971bd
Rahma Maalej <rahma.maalej.13@gmail.com>
2024-02-10 22:51:38 +0100

    quelques MAJs a propos my hello toas app, tp1

--- hellotoast/app/src/main/java/com/example/hellotoast/MainActivity.java
+++ hellotoast/app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.hellotoast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.os.Bundle;
@@ -8,12 +10,16 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     private int mCount=0;
+
     private TextView mShowCount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.show_count);
+
+
     }
 
     public void showToast(View view) {
@@ -29,4 +35,19 @@ public class MainActivity extends AppCompatActivity {
         if (mShowCount != null)
             mShowCount.setText(Integer.toString(mCount));
     }
+
+    @SuppressLint("SetTextI18n")
+    public void showButtonReset(View view) {
+        mCount=0;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+    }
+
+    public void showButtonSub(View view) {
+        --mCount;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+    }
+
+
 }
